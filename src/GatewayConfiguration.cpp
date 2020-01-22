b1dc810fc3618499171577df76df0f1ed735e7a1
Squall-DA <Squall-DA@users.noreply.github.com>
2020-01-22 12:58:13 -0500

    Add new source files and libraries

---
+++ src/GatewayConfiguration.cpp
@@ -0,0 +1,10 @@
+/** 
+ *  @file                   GatewayConfiguration.cpp
+ *  @brief                  Handles the configuration and interfacing to IotWebConf
+ *  @copyright              2020 Squall-DA
+ *  @date                   01/22/2020
+ * 
+ *  @remark Author:         Squall-DA
+ *  @remark Project Tree:   LoRaRemoteStartGW
+ * 
+ */
---
+++ src/MQTTHandler.cpp
@@ -0,0 +1,12 @@
+/** 
+ *  @file                   MQTTHandler.cpp
+ *  @brief                  Handles all MQTT logic for the gateway
+ *  @copyright              2020 Squall-DA
+ *  @date                   01/22/2020
+ * 
+ *  @remark Author:         Squall-DA
+ *  @remark Project Tree:   LoRaRemoteStartGW
+ * 
+ */
+
+ 
